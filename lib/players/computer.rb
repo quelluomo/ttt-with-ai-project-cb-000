7b3800ecde7e95299cf0ae1b010ac754d75ec56f
Matteo Muscas <thatman.j@gmail.com>
2019-01-23 09:19:23 +0000

    Automatically backed up by Learn

--- lib/players/computer.rb
+++ lib/players/computer.rb
@@ -8,25 +8,25 @@ module Players
 
     #if computer is X start taking 5, if it's O and 5 is not taken, take 5:
     if !board.taken?(5)
-    move = "5"
+      move = "5"
     #if 5 is taken and computer is O, move to any corner:
     elsif board.turn_count == 1
-    move = "1" || "3" || "7" || "9"
+      move = "1" || "3" || "7" || "9"
     #if computer is X and O takes 1, move to 9:
     elsif board.turn_count == 2 && board.position(1)
-    move = "9"
+      move = "9"
     #if computer is X and O takes 3, move to 7:
     elsif board.turn_count == 2 && board.position(3)
-    move = "7"
+      move = "7"
     #if computer is X and O takes 7, move to 3:
     elsif board.turn_count == 2 && board.position(7)
-    move = "3"
+      move = "3"
     #if computer is X and O takes 9, move to 1:
     elsif board.turn_count == 2 && board.position(9)
-    move = "1"
+      move = "1"
     #if computer is O and started from 5, if X is in the opposite corners, move to 2.
     elsif board.turn_count == 3 && (board.position(1) == board.position(9) || board.position(3) == board.position(7))
-    move = "2"
+      move = "2"
     #now try to win by completing the winning combination (if you have 2 out of 3 positions taken of the combo) or block the winning chance of the opponent:
     else
       Game::WIN_COMBINATIONS.detect do |combo|
