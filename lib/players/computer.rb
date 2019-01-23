31e049473572a3f4389dfe2b80fbb7a7d2239d1b
Matteo Muscas <thatman.j@gmail.com>
2019-01-23 09:28:41 +0000

    Automatically backed up by Learn

--- lib/players/computer.rb
+++ lib/players/computer.rb
@@ -14,16 +14,16 @@ module Players
       move = "1" # || "3" || "7" || "9" <- this is the right strategy, but the game crashes
     #if computer is X and O takes 1, move to 9:
     elsif board.turn_count == 2 && board.position(1)
-      move = "9"
+      move = corner.sample
     #if computer is X and O takes 3, move to 7:
     elsif board.turn_count == 2 && board.position(3)
-      move = "7"
+      move = corner.sample
     #if computer is X and O takes 7, move to 3:
     elsif board.turn_count == 2 && board.position(7)
-      move = "3"
+      move = corner.sample
     #if computer is X and O takes 9, move to 1:
     elsif board.turn_count == 2 && board.position(9)
-      move = "1"
+      move = corner.sample
     #if computer is O and started from 5, if X is in the opposite corners, move to 2.
     elsif board.turn_count == 3 && (board.position(1) == board.position(9) || board.position(3) == board.position(7))
       move = "2"
